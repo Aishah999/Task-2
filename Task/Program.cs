e45a091c75411df56ea0179c4b3486030b62a5f3
Aishah999 <aishaah@code.edu.az>
2024-10-26 12:41:36 +0400

    first commit

---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //SumOfEven(10);
+        //Console.WriteLine(SumOfEven(10));
+        //Sum(22);
+    }
+    //static int SumOfEven(int number)
+    //{ int i = 0;
+        //int Sum =0;
+        //for(i=1;i<=number;i++)
+        //{
+            //if(i%2==0)
+            //{
+                //Sum += i;
+                
+            //}
+           
+        //}
+        //return Sum;
+    //}
+    //static void Sum(int num)
+    //{
+        //int i = 0;
+        //int Sum = 0;
+       // for(i=1;i<=num;i++)
+        //{
+            //if(i%2==0)
+            //{
+                //Sum += i;
+                
+            //}
+        //}
+        //Console.WriteLine(Sum);
+
+   // }
+}
+
